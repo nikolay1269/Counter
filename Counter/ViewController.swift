1706ce77298b1178e89da3a4b1f401d313d62078
Nikolay Shpilevskiy <nikolay.shpilevskiy@sibers.com>
2024-05-07 14:33:18 +0500

    Task completed

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var historyTextView: UITextView!
+    
+    private var counter: UInt = 0
+    
+    @IBAction func plusButtonDidTap() {
+        counter += 1
+        counterLabel.text = "Значение счётчика: \(counter)"
+        addHistoryRecord(text: "\(currentDateTime()): значение изменено на +1\n")
     }
-
-
+    
+    @IBAction func minusButtonDidTap(_ sender: Any) {
+        if counter > 0 {
+            counter -= 1
+            counterLabel.text = "Значение счётчика: \(counter)"
+            addHistoryRecord(text: "\(currentDateTime()): значение изменено на -1\n")
+        } else {
+            addHistoryRecord(text: "\(currentDateTime()): попытка уменьшить значение счётчика меньше 0\n")
+        }
+    }
+    
+    @IBAction func resetButtonDidTap(_ sender: Any) {
+        counter = 0
+        counterLabel.text = "0"
+        addHistoryRecord(text: "\(currentDateTime()): значение сброшено\n")
+    }
+    
+    private func addHistoryRecord(text: String) {
+        historyTextView.insertText(text)
+    }
+    
+    private func currentDateTime() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let dateString = dateFormatter.string(from: Date())
+        return dateString
+    }
 }
-
